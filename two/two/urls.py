72174ccf1cbf2676712a9afb28422d2b0431a23f
zzf531 <2315519934@qq.com>
2019-06-20 21:26:04 +0800

    delete two

--- two/two/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'', include('blog.urls'))
-]
